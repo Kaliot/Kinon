021b6cf8e4dc04518019bf2b293c6452f40cbcbf
Alexander <abolunev@gmail.com>
2022-09-13 18:40:05 +0300

    Added night theme, top bar, bottom navigation bar Removed menu buttons

--- app/src/main/java/com/bolunevdev/kinon/MainActivity.kt
+++ app/src/main/java/com/bolunevdev/kinon/MainActivity.kt
@@ -5,58 +5,81 @@ import android.net.Uri
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import com.bolunevdev.kinon.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        button_search.setOnClickListener {
-            Toast.makeText(this, "Поиск", Toast.LENGTH_SHORT).show()
-        }
-
-        button_new.setOnClickListener {
-            Toast.makeText(this, "Новинки", Toast.LENGTH_SHORT).show()
-        }
-
-        button_favorites.setOnClickListener {
-            Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
-        }
-
-        button_recommended.setOnClickListener {
-            Toast.makeText(this, "Рекомендованное", Toast.LENGTH_SHORT).show()
-        }
-
-        button_settings.setOnClickListener {
-            Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
-        }
-
-        poster.setOnClickListener {
-            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.imdb.com/title/tt12873562/"))
+        binding.poster.setOnClickListener {
+            val browserIntent =
+                Intent(Intent.ACTION_VIEW, Uri.parse("https://www.imdb.com/title/tt12873562/"))
             startActivity(browserIntent)
         }
 
-        poster2.setOnClickListener {
-            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.imdb.com/title/tt12593682/"))
+        binding.poster2.setOnClickListener {
+            val browserIntent =
+                Intent(Intent.ACTION_VIEW, Uri.parse("https://www.imdb.com/title/tt12593682/"))
             startActivity(browserIntent)
         }
 
-        poster3.setOnClickListener {
-            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.imdb.com/title/tt13223398/"))
+        binding.poster3.setOnClickListener {
+            val browserIntent =
+                Intent(Intent.ACTION_VIEW, Uri.parse("https://www.imdb.com/title/tt13223398/"))
             startActivity(browserIntent)
         }
 
-        poster4.setOnClickListener {
-            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.imdb.com/title/tt1745960/"))
+        binding.poster4.setOnClickListener {
+            val browserIntent =
+                Intent(Intent.ACTION_VIEW, Uri.parse("https://www.imdb.com/title/tt1745960/"))
             startActivity(browserIntent)
         }
 
-        poster5.setOnClickListener {
-            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.imdb.com/title/tt14614892/"))
+        binding.poster5.setOnClickListener {
+            val browserIntent =
+                Intent(Intent.ACTION_VIEW, Uri.parse("https://www.imdb.com/title/tt14614892/"))
             startActivity(browserIntent)
+        }
+
+        binding.topAppBar.setNavigationOnClickListener {
+            Toast.makeText(this, "Меню", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.topAppBar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.settings -> {
+                    Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.search -> {
+                    Toast.makeText(this, "Поиск", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+        }
+
+        binding.bottomNavigation.setOnItemReselectedListener {
+            when (it.itemId) {
+                R.id.new_titles -> {
+                    Toast.makeText(this, "Новинки", Toast.LENGTH_SHORT).show()
+                }
+                R.id.favorites -> {
+                    Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+                }
+                R.id.recommended -> {
+                    Toast.makeText(this, "Рекомендованное", Toast.LENGTH_SHORT).show()
+                }
+            }
         }
     }
-
 }
+
+

7210eaa35df2d74b375b8d4fdb35fc5ed8b797af
Kaliot <48630229+Kaliot@users.noreply.github.com>
2023-02-23 03:37:46 +0300

    Added poster download button Implemented the logic for handling the poster loading error

--- app/src/main/java/com/bolunevdev/kinon/view/fragments/DetailsFragment.kt
+++ app/src/main/java/com/bolunevdev/kinon/view/fragments/DetailsFragment.kt
@@ -1,13 +1,22 @@
 package com.bolunevdev.kinon.view.fragments
 
+import android.Manifest
+import android.content.ContentValues
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
 import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.transition.Fade
@@ -23,6 +32,8 @@ import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.target.Target
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.*
 
 
 class DetailsFragment : Fragment() {
@@ -61,6 +72,8 @@ class DetailsFragment : Fragment() {
 
         setDetails()
 
+        initDetailsFabDownloadWp()
+
         initDetailsFabShare()
     }
 
@@ -79,6 +92,12 @@ class DetailsFragment : Fragment() {
             } else {
                 viewModel.deleteFromFavoritesFilms(film.id)
             }
+        }
+    }
+
+    private fun initDetailsFabDownloadWp() {
+        binding.detailsFabDownloadWp.setOnClickListener {
+            performAsyncLoadOfPoster()
         }
     }
 
@@ -114,6 +133,7 @@ class DetailsFragment : Fragment() {
             ) as Film
         } else {
             //Получаем наш фильм из переданного бандла
+            @Suppress("DEPRECATION")
             arguments?.getParcelable<Film>(MainActivity.KEY_FILM_DETAILS_FRAGMENT) as Film
         }
 
@@ -161,8 +181,123 @@ class DetailsFragment : Fragment() {
         )
     }
 
+    private fun saveToGallery(bitmap: Bitmap) {
+        //Проверяем версию системы
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            //Создаем объект для передачи данных
+            val contentValues = ContentValues().apply {
+                //Составляем информацию для файла (имя, тип, дата создания, куда сохранять и т.д.)
+                put(MediaStore.Images.Media.TITLE, film.title.handleSingleQuote())
+                put(
+                    MediaStore.Images.Media.DISPLAY_NAME,
+                    film.title.handleSingleQuote()
+                )
+                put(MediaStore.Images.Media.MIME_TYPE, IMAGE_TYPE)
+                put(
+                    MediaStore.Images.Media.DATE_ADDED,
+                    System.currentTimeMillis() / MILLIS_TO_SEC_RATIO
+                )
+                put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis())
+                put(MediaStore.Images.Media.RELATIVE_PATH, DOWNLOAD_PATH)
+            }
+            //Получаем ссылку на объект Content resolver, который помогает передавать информацию из приложения вовне
+            val contentResolver = requireActivity().contentResolver
+            val uri = contentResolver.insert(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                contentValues
+            )
+            //Открываем канал для записи на диск
+            val outputStream = contentResolver.openOutputStream(uri!!)
+            //Передаем нашу картинку, может сделать компрессию
+            bitmap.compress(Bitmap.CompressFormat.JPEG, PERCENT_OF_QUALITY, outputStream)
+            //Закрываем поток
+            outputStream?.close()
+        } else {
+            //То же, но для более старых версий ОС
+            @Suppress("DEPRECATION")
+            MediaStore.Images.Media.insertImage(
+                requireActivity().contentResolver,
+                bitmap,
+                film.title.handleSingleQuote(),
+                film.description.handleSingleQuote()
+            )
+        }
+    }
+
+    private fun String.handleSingleQuote(): String {
+        return this.replace("'", "")
+    }
+
+    private fun performAsyncLoadOfPoster() {
+        //Проверяем есть ли разрешение
+        if (!checkPermission()) {
+            //Если нет, то запрашиваем и выходим из метода
+            requestPermission()
+            return
+        }
+        //Создаем родительский скоуп с диспатчером Main потока, так как будем взаимодействовать с UI
+        MainScope().launch {
+            //Включаем Прогресс-бар
+            binding.progressBar.isVisible = true
+            //Создаем через async, так как нам нужен результат от работы, то есть Bitmap
+            val scope = CoroutineScope(Dispatchers.IO)
+            val job = scope.async {
+                viewModel.loadWallpaper(ApiConstants.IMAGES_URL + "original" + film.poster)
+            }
+            //Сохраняем в галерею, как только файл загрузится
+            try {
+                saveToGallery(job.await())
+                //Выводим снекбар с кнопкой перейти в галерею
+                Snackbar.make(
+                    binding.root,
+                    R.string.downloaded_to_gallery,
+                    Snackbar.LENGTH_LONG
+                )
+                    .setAction(R.string.open) {
+                        val intent = Intent()
+                        intent.action = Intent.ACTION_VIEW
+                        intent.type = "image/*"
+                        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                        startActivity(intent)
+                    }
+                    .show()
+            } catch (_: Exception) {
+                Toast.makeText(
+                    requireContext(),
+                    "Не удалось скачать изображение!",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            //Отключаем Прогресс-бар
+            binding.progressBar.isVisible = false
+        }
+    }
+
+    //Узнаем, было ли получено разрешение ранее
+    private fun checkPermission(): Boolean {
+        val result = ContextCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+        return result == PackageManager.PERMISSION_GRANTED
+    }
+
+    //Запрашиваем разрешение
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            requireActivity(),
+            arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+            WRITE_EXTERNAL_STORAGE_PERMISSION_CODE
+        )
+    }
+
     companion object {
         private const val MIME_TYPE = "text/plain"
         private const val IMAGE_SIZE = "w780"
+        private const val MILLIS_TO_SEC_RATIO = 1000
+        private const val IMAGE_TYPE = "image/jpeg"
+        private const val DOWNLOAD_PATH = "Pictures/Kinon"
+        private const val PERCENT_OF_QUALITY = 100
+        private const val WRITE_EXTERNAL_STORAGE_PERMISSION_CODE = 1
     }
 }
--- app/src/main/java/com/bolunevdev/kinon/viewmodel/DetailsFragmentViewModel.kt
+++ app/src/main/java/com/bolunevdev/kinon/viewmodel/DetailsFragmentViewModel.kt
@@ -1,12 +1,17 @@
 package com.bolunevdev.kinon.viewmodel
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.bolunevdev.kinon.App
 import com.bolunevdev.kinon.data.entity.Film
 import com.bolunevdev.kinon.domain.Interactor
+import java.net.URL
 import java.util.concurrent.Executors
 import javax.inject.Inject
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 class DetailsFragmentViewModel : ViewModel() {
     val filmsListLiveData: LiveData<List<Film>>
@@ -29,6 +34,14 @@ class DetailsFragmentViewModel : ViewModel() {
     fun deleteFromFavoritesFilms(id: Int) {
         Executors.newSingleThreadExecutor().execute {
             interactor.setFilmAsNotFavoriteInDB(id)
+        }
+    }
+
+    suspend fun loadWallpaper(url: String): Bitmap {
+        return suspendCoroutine {
+            val posterUrl = URL(url)
+            val bitmap = BitmapFactory.decodeStream(posterUrl.openConnection().getInputStream())
+            it.resume(bitmap)
         }
     }
 }
